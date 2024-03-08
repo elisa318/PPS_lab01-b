c385e29afd6be9ec9d91172c6c6485a695e90334
elisa318 <elisa.barberini3@studio.unibo.it>
2024-03-08 18:38:07 +0100

    tdd terzo step, refactorin LogicImpl

--- src/main/java/e1/LogicsImpl.java
+++ src/main/java/e1/LogicsImpl.java
@@ -8,6 +8,8 @@ public class LogicsImpl implements Logics {
 	private Pair<Integer,Integer> knight;
 	private final RandomEmptyPairInitializer randomPair;
 	private final int size;
+	private static int ABS_VALUE = 3;
+	private static int START_INDEX = 0;
 	 
     public LogicsImpl(int size){
     	this.size = size;
@@ -18,13 +20,13 @@ public class LogicsImpl implements Logics {
     
 	@Override
 	public boolean hit(int row, int col) {
-		if (row<0 || col<0 || row >= this.size || col >= this.size) {
+		if (row<START_INDEX || col<START_INDEX || row >= this.size || col >= this.size) {
 			throw new IndexOutOfBoundsException();
 		}
 		// Below a compact way to express allowed moves for the knight
-		int x = row-this.knight.getX();
-		int y = col-this.knight.getY();
-		if (x!=0 && y!=0 && Math.abs(x)+Math.abs(y)==3) {
+		int x = this.getXValue(row);
+		int y = this.getYValue(col);
+		if (x!=START_INDEX && y!=START_INDEX && this.getAbsCondition(x, y)) {
 			this.knight = new Pair<>(row,col);
 			return this.pawn.equals(this.knight);
 		}
@@ -39,5 +41,17 @@ public class LogicsImpl implements Logics {
 	@Override
 	public boolean hasPawn(int row, int col) {
 		return this.pawn.equals(new Pair<>(row,col));
+	}
+
+	private boolean getAbsCondition(int x, int y) {
+		return Math.abs(x)+Math.abs(y)==ABS_VALUE;
+	}
+
+	private int getXValue(int row) {
+		return row-this.knight.getX();
+	}
+
+	private int getYValue(int col) {
+		return col-this.knight.getY();
 	}
 }
